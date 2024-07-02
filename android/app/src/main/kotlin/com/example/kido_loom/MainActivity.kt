c43106e3de642d83036ec539080ed49b4e135e5e
aliahmad772002 <aliahmad772002@gmail.com>
2024-07-02 11:05:26 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/kido_loom/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kido_loom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final bgColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    Color(0xFFF3FFFF),
+    Color(0xffC8FFFD),
+  ],
+);
+final borderColor = Color(0xff18D0CC);
+final whiteColor = Color(0xffFFFFEA);
+final txt1 = Color(0xff58508D);
+final txt2 = Color(0xff4D4D4D);
+final circleColor = Color(0xffD3D3D3);
